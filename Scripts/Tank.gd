e1140a4fd3e74e98a4cc116fca6361fd27cdc0d5
john <silvamatheusjonathan@gmail.com>
2020-06-29 14:34:13 -0300

    Primeiro comite

---
+++ Scripts/Tank.gd
@@ -0,0 +1,35 @@
+extends KinematicBody2D
+
+
+var speed = 200
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+	var dir_x = 0
+	var dir_y = 0
+	
+	if Input.is_action_pressed("ui_right"):
+		dir_x += 1
+		pass
+	
+	if Input.is_action_pressed("ui_left"):
+		dir_x -= 1
+		pass
+	
+	if Input.is_action_pressed("ui_up"):
+		dir_y -= 1
+		pass
+	
+	if Input.is_action_pressed("ui_down"):
+		dir_y += 1
+		pass
+	
+	translate(Vector2(dir_x , dir_y) * delta * speed)
+	pass
